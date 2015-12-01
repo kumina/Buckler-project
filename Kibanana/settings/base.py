1464dbb1c2e2eb9b62dfee197d16d2a6ba90659c
Ivo van der Wijk <github@in.m3r.nl>
2015-12-01 16:08:27 +0100

    extra testing account; flake8

--- Kibanana/settings/base.py
+++ Kibanana/settings/base.py
@@ -1,20 +1,22 @@
-from .django_base import *
+from .django_base import *  # NOQA
 
-## Sample / default config
+# Sample / default config
 
-CONFIG = { 'ivo': {'password':'1v0',
-                   'indexes': ('logstash-ivo-*',)},
-           'test': {'password': 't3st',
-                    'indexes': ('accounts', )},
-           'superuser': {'password': 'geheim',
-                         'indexes':('accounts', 'logstash-*', 'logstash-ivo-*')}
-         }
+CONFIG = {'ivo': {'password': '1v0',
+                  'indexes': ('logstash-ivo-*',),
+                  'poweruser': True},
+          'test': {'password': 't3st',
+                   'indexes': ('accounts', )},
+          'superuser': {'password': 'geheim',
+                        'indexes': ('accounts', 'logstash-*',
+                                    'logstash-ivo-*')
+                        }
+          }
 
 KIBANA_UPSTREAM = "http://kibana:5601"
 ES_UPSTREAM = "http://es:9200"
 
 try:
-    from .config import *
+    from .config import *  # NOQA
 except ImportError:
     pass
-
