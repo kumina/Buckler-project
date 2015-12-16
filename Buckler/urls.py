998fcf912b8b48510b4684d71b17736443856fbf
Ivo van der Wijk <github@in.m3r.nl>
2015-12-16 11:04:11 +0100

    Change name to buckler

--- Buckler/urls.py
+++ Buckler/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
-import kibanana.urls
+import buckler.urls
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
