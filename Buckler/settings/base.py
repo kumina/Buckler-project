bcfdb2741f25396ab1baf483589c76b677e63609
Ed Schouten <ed@kumina.nl>
2016-08-30 16:16:41 +0200

    Update configurations to match the latest implementation of Buckler.

--- Buckler/settings/base.py
+++ Buckler/settings/base.py
@@ -1,20 +1,26 @@
 from .django_base import *  # NOQA
+import crypt
 
 # Sample / default config
 
-CONFIG = {'ivo': {'password': '1v0',
+def hash_passwd(s):
+    return crypt.crypt(s, '$1$xyz')
+
+CONFIG = {'ivo': {'password': hash_passwd('1v0'),
                   'indexes': ('logstash-ivo-*',),
-                  'poweruser': True},
-          'test': {'password': 't3st',
-                   'indexes': ('accounts', )},
-          'superuser': {'password': 'geheim',
+                  'userdata_index': 'ivo'},
+          'test': {'password': hash_passwd('t3st'),
+                   'indexes': ('accounts', ),
+                   'userdata_index': 'test'},
+          'superuser': {'password': hash_passwd('geheim'),
                         'indexes': ('accounts', 'logstash-*',
-                                    'logstash-ivo-*')
-                        }
+                                    'logstash-ivo-*'),
+                        'userdata_index': 'superuser'},
           }
 
 KIBANA_UPSTREAM = "http://kibana:5601"
 ES_UPSTREAM = "http://es:9200"
+ES_USERDATA_PREFIX = '.kibana'
 
 try:
     from .config import *  # NOQA
